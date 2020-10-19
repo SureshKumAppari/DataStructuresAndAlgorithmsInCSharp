647b15b4f605d0eeab7d8c0c117938e127a8b1b7
SureshKumAppari <suri.trojan@gmail.com>
2020-10-19 12:33:33 +0530

    RoundA
    RoundA

---
+++ Allocation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeConsole
+{
+    class Allocation
+    {
+        static int n, b;
+        static int ans = 0;
+
+        public static void notMain(string[] args)
+        {
+            int t;
+            t = Convert.ToInt32(Console.ReadLine());
+            int tc = 0;
+            while (t-- > 0)
+            {
+                tc++;
+                n = Convert.ToInt32(Console.ReadLine());
+                int[] a = new int[n];
+                b = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < n; i++)
+                {
+                    a[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                Array.Sort(a);
+                ans = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (a[i] <= b)
+                    {
+                        ans++;
+                        b -= a[i];
+                    }
+                }
+                Console.WriteLine("Case #" + tc + ": " + ans);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Plates.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeConsole
+{
+    class Plates
+    {
+        private static void notMain(string[] args)
+        {
+            int T;
+            T = Convert.ToInt32(Console.ReadLine());
+            for (int test = 1; test <= T; test++)
+            {
+                int n, k, p;
+                n = Convert.ToInt32(Console.ReadLine());
+                k = Convert.ToInt32(Console.ReadLine());
+                p = Convert.ToInt32(Console.ReadLine());
+                List<List<int>> a = NestedList<int>(n, k);
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < k; j++)
+                    {
+                        a[i][j] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+
+                List<int> dp = new List<int>(p + 1);
+                for (int i = 0; i < n; i++)
+                {
+                    List<int> ndp = new List<int>(dp);
+                    for (int j = 0; j <= p; j++)
+                    {
+                        int cur = 0;
+                        for (int l = 0; l <= k && j + l <= p; l++)
+                        {
+                            ndp[j + l] = Math.Max(ndp[j + l], dp[j] + cur);
+                            if (l < k)
+                            {
+                                cur += a[i][l];
+                            }
+                        }
+                    }
+                    dp = ndp;
+                }
+
+                Console.WriteLine("Case #%d: %d\n", test, dp[p]);
+                Console.ReadLine();
+            }
+
+            static List<List<T>> NestedList<T>(int outerSize, int innerSize)
+            {
+                List<List<T>> temp = new List<List<T>>();
+                for (int count = 1; count <= outerSize; count++)
+                {
+                    temp.Add(new List<T>(innerSize));
+                }
+
+                return temp;
+            }
+        }
+    }
+}
